9b17b05ce0be6c9dbc4101aa366614031913f1f3
ilmancio98 <f.mancini699@gmail.com>
2020-12-14 14:45:44 +0100

    First commit

---
+++ app/src/main/java/com/example/fafeat/LoginSignup/FullscreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.fafeat.LoginSignup;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.example.fafeat.R;
+
+public class FullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_fullscreen);
+    }
+
+    public void callLoginScreen(View view){
+
+        Intent intent = new Intent(getApplicationContext(),Login.class);
+
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.login_btn), "transition_login");
+
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation( FullscreenActivity.this, pairs);
+        startActivity(intent, options.toBundle());
+    }
+
+}
